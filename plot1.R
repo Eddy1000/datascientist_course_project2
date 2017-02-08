467574242851e7f2f50be42889b5ffd0f629bc88
Alexander Eddelbuettel <Alexander@Eddelbuettel.eu>
2017-02-08 17:02:41 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,5 @@
+#Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+
 #Fetching the Data if needed
 filename1 <- "summarySCC_PM25.rds"
 filename2 <- "Source_Classification_Code.rds"
